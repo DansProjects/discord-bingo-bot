f754d50f58e89f60e1a04443ddc7ecc64384d689
Dan Mo <danmo1337@gmail.com>
2017-06-15 19:41:53 -0400

    command updates

--- WoWBingoBot.py
+++ WoWBingoBot.py
@@ -11,12 +11,18 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('?hello'):
+    if message.content.startswith('?bingo'):
         msg = 'Hello {0.author.mention}'.format(message)
         await client.send_message(message.channel, msg)
         await client.send_message(message.channel, '-------------')
         #generate and send image to discord
         await client.send_file(message.channel, bingo.generate_board())
+    elif message.content.startswith('?about'):
+        msg = 'Bingo bot for dank memes...by Dan'
+        await client.send_message(message.channel, msg)
+    elif message.content.startswith('?cheats'):
+        msg = 'Give Dan gold for hellfire deck upgrade'
+        await client.send_message(message.channel, msg)
 
 @client.event
 async def on_ready():
