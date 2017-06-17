33da99472ee89bbd8e522fbe120562a7bdfa937e
pancan <pancan@pancan.pancan>
2017-06-17 12:09:01 -0700

    changed x = x + y to x += y

--- BingoBoard.py
+++ BingoBoard.py
@@ -34,10 +34,10 @@ class BingoBoard:
                 #make sure no line goes past 13 characters
                 text = textwrap.fill(cell[0], 13)
                 draw.text((x, y), text, 'blue', font)
-                x = x + cellWidth
+                x += cellWidth
 
             x = initialXPosition
-            y = y + cellHeight
+            y += cellHeight
 
         file_dir = 'bingo_boards/'
 
